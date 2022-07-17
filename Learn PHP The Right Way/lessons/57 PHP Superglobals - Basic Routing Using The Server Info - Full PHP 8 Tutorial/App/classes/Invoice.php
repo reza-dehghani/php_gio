79addd8ace40ca8912917471fd0ce7ab94211634
reza-dehghani <iamreza@outlook.com>
2022-07-17 20:23:27 +0430

    Lesson 57 added

---
+++ Learn PHP The Right Way/lessons/57 PHP Superglobals - Basic Routing Using The Server Info - Full PHP 8 Tutorial/App/classes/Invoice.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Classes;
+
+class invoice
+{
+    public function index(): string
+    {
+        return 'Invoice';
+    }
+
+    public function create(): string
+    {
+        return 'Create Invoice';
+    }
+}
---
+++ Learn PHP The Right Way/lessons/57 PHP Superglobals - Basic Routing Using The Server Info - Full PHP 8 Tutorial/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// echo '<pre>';
+// print_r($_SERVER);
+// echo '<pre>';
+
+$router = new App\Router();
+
+$router
+->register('/',[App\Classes\Home::class, 'index'])
+->register('/invoices',[App\Classes\Invoice::class, 'index'])
+->register('/invoices/create',[App\Classes\Invoice::class, 'create']);
+
+echo $router->resolve($_SERVER['REQUEST_URI']);
